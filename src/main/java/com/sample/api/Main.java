f5c7f92ba6c89c5dadf52efd0dc0ee9a249b654d
AlexeevAlexandr <fart82@ukr.net>
2019-09-30 10:55:39 +0300

    changed spring Schedule instead TimerTask

--- src/main/java/com/sample/api/Main.java
+++ src/main/java/com/sample/api/Main.java
@@ -1,16 +1,13 @@
 package com.sample.api;
 
-import com.sample.api.controller.timer.MyTimerTask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Timer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        Timer timer = new Timer(true);
-        timer.scheduleAtFixedRate(new MyTimerTask(), 0, 30*1000);
     }
 }
---
+++ src/main/java/com/sample/api/controller/timer/Schedule.java
@@ -0,0 +1,33 @@
+package com.sample.api.controller.timer;
+
+import com.sample.api.service.TodoService;
+import lombok.extern.java.Log;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log
+public class Schedule {
+
+    final private TodoService todoService;
+
+    public Schedule(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    // set schedule at 6:00 AM everyday
+    @Scheduled(cron = "0 0 6 ? * *")
+    public void run() {
+        scheduledTask();
+    }
+
+    private void scheduledTask() {
+        log.info("Attempt to delete all completed tasks");
+        try {
+            int count = todoService.deleteCompletedTodo();
+            log.info("Completed tasks have been deleted : " + count);
+        } catch (Exception e){
+            log.warning("Can not delete completed tasks");
+        }
+    }
+}
